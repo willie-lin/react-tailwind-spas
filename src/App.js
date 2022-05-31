369c572e62750b45bddd78bd577770827edd6bd1
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-05-31 23:52:35 +0800

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
-<<<<<<< HEAD
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 import Navbar from './components/Navbar'
-
+import Hero from "./components/Hero";
 
 
 function App() {
   return (
     <div>
       <Navbar />
->>>>>>> a3c69a4 (Initialize project using Create React App)
+        <Hero />
     </div>
   );
 }

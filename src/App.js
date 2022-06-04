390bd43be4ac8fed8e26568c49bc7edc3a3193fb
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-06-04 21:24:12 +0800

    update hero

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import Hero from "./components/Hero";
+import Analytics from "./components/Analytics";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar />
         <Hero />
+        <Analytics />
     </div>
   );
 }
---
+++ src/components/Analytics.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Laptop from '../assets/laptop.jpg'
+
+const Analytics = () => {
+    return (
+        <div className='w-full bg-white py-16 px-4'>
+            <div className='max-w-[1240] mx-auto grid md:grid-cols-2'>
+                <img src={Laptop} alt="/" />
+            </div>
+
+        </div>
+    )
+}
+export default Analytics

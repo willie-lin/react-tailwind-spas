d8d1854edd95748c1dbe9d18af109f22b93e69d1
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-06-04 16:23:02 +0800

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,10 +19,9 @@ const Navbar = () => {
                 <li className='p-4'>Contact</li>
             </ul>
             <div onClick={handleNav} className='block md:hidden'>
-                {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} /> }
+                {nav ? <AiOutlineClose size={20}/>:<AiOutlineMenu size={20} /> }
             </div>
-            <ul className={nav ? 'flex lift-0 top-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500':
-                'ease-in-out duration-500 fixed lift-[-100%]'}>
+            <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4' >REACT.</h1>
                     <li className='p-4 border-b border-gray-600'>Home</li>
                     <li className='p-4 border-b border-gray-600'>Company</li>

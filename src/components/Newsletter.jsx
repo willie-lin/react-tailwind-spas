9b8e297b385df1f826e19d7eb1c0567dad7b5745
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-06-05 23:26:52 +0800

    add Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Newsletter = () => {
     return (
-        <div className='w-full py-16 text-white'>
+        <div className='w-full py-16 text-white px-4'>
             <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
                 <div className='lg:col-span-2'>
                     <h1 className='md:text-4xl sm:text-3xl text-xl font-bold py-2'>Want tips & tricks to optimize your flow?</h1>
@@ -12,8 +12,8 @@ const Newsletter = () => {
                         <input className='p-3 flex w-full rounded-md text-black' type="email" placeholder='Enter Email' />
                         <button className='bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 py-3'>Notify Me</button>
                     </div>
-
                 </div>
+                <p>We care bout the protection of your data. Read our Privacy Policy.</p>
             </div>
 
         </div>

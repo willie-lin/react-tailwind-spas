6a4d311b9fbdafda54f3765c899176b396985a7b
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-06-06 00:19:09 +0800

    add Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -19,8 +19,8 @@ const Cards = () => {
                     </div>
                     <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
                 </div>
-                <div className='w-full shadow-xl flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
-                    <img className='w-20 mx-auto bg-white' src={Double} alt="/" />
+                <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
+                    <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
                     <h2 className='text-2xl font-bold text-center py-8'>Double User</h2>
                     <p className='text-center text-4xl font-bold'>$149</p>
                     <div className='text-center font-medium'>
@@ -30,8 +30,8 @@ const Cards = () => {
                     </div>
                     <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
                 </div>
-                <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-                    <img className='w-20 mx-auto bg-white' src={Triple} alt="/" />
+                <div className='w-full shadow-xl  flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                    <img className='w-20 mx-auto  bg-white' src={Triple} alt="/" />
                     <h2 className='text-2xl font-bold text-center py-8'>Triple User</h2>
                     <p className='text-center text-4xl font-bold'>$149</p>
                     <div className='text-center font-medium'>

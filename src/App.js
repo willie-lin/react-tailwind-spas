a5b53716cf7ef50efbe0b50086105a1e42ed663d
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-06-06 00:08:57 +0800

    add Newsletter.jsx

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
 import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Analytics />
       <Newsletter />
+        <Cards />
     </div>
   );
 }

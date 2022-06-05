bb82d9887b4729a5996dcd69854bef8d15c36429
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-06-06 00:10:41 +0800

    add Newsletter.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -19,7 +19,7 @@ const Cards = () => {
                     </div>
                     <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
                 </div>
-                <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                <div className='w-full shadow-xl flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
                     <img className='w-20 mx-auto bg-white' src={Double} alt="/" />
                     <h2 className='text-2xl font-bold text-center py-8'>Double User</h2>
                     <p className='text-center text-4xl font-bold'>$149</p>
